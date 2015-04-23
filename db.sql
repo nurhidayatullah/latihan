f68e7af2702aff065352f1ae30e40be34d6d5d17
nurhidayatullah <nur_hidayat_45@yahoo.com>
2015-04-23 22:27:53 +0700

    database

---
+++ db.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 5.6.16 : Database - latihan_ci
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`latihan_ci` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `latihan_ci`;
+
+/*Table structure for table `siswa` */
+
+DROP TABLE IF EXISTS `siswa`;
+
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `kelas` varchar(5) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `siswa` */
+
+insert  into `siswa`(`id`,`nama`,`kelas`,`alamat`) values (1,'Rian Tri cahyono','D','Tuban'),(2,'sotak','D','sidoarjo');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
